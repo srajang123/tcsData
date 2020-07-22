330f503a1d259ec5d055ec69ac9bc165c2ce1aa7
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-07-22 23:16:23 +0530

    Added Validation

--- src/Components/FormData.js
+++ src/Components/FormData.js
@@ -3,6 +3,32 @@ import {TextField,RaisedButton,AppBar} from 'material-ui';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 export default class FormData extends Component
 {
+    chk=()=>{
+        const {fname,lname,Fname,Mname,city,role,email,contact}=this.props.data;
+        const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if(fname==='')
+            alert('Please Enter First Name');
+        else if(lname==='')
+            alert('Please Enter Last Name');
+        else if(Fname==='')
+            alert("Please Enter Father's Name");
+        else if(Mname==='')
+            alert("Please Enter Mother's Name");
+        else if(city==='')
+            alert('Please enter city');
+        else if(role==='')
+            alert('Please enter role');
+        else if(email==='')
+            alert('Please Enter Email');
+        else if(contact==='')
+            alert('Please Enter Contact Number');
+        else if(isNaN(Number(contact))||contact.length!==10)
+            alert('Please Enter Valid Contact Number');
+        else if(!email.match(mailformat))
+            alert('Please Enter Valid Email');
+        else
+            this.props.add();
+    }
     render=()=>{
     return(
         <MuiThemeProvider>
@@ -17,7 +43,7 @@ export default class FormData extends Component
                     <TextField hintText='Enter Your Role' floatingLabelText='Role' defaultValue={this.props.role} onChange={this.props.change('role')}/><br/>
                     <TextField hintText='Enter Your Email' floatingLabelText='E-mail' defaultValue={this.props.email} onChange={this.props.change('email')}/><br/>
                     <TextField hintText='Enter Your contact no' floatingLabelText='Contact No' defaultValue={this.props.contact} onChange={this.props.change('contact')}/><br/>
-                    <RaisedButton label='Submit' onClick={this.props.add} primary={true}/>
+                    <RaisedButton label='Submit' onClick={this.chk} primary={true}/>
                 </div>
         </React.Fragment>
         </MuiThemeProvider>
